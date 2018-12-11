8d2068e809cb0d2a341a788de781d78566e88b8b
Dhiran08 <dhiranandi08@gmail.com>
2018-12-11 16:46:04 +0700

    menambah dokumentasi

--- src/App.js
+++ src/App.js
@@ -23,3 +23,45 @@ class App extends Component {
 }
 
 export default App;
+
+// dokumentasi
+
+/*
+(1). Detik dalam react?
+mas baron, kemarin saya menemukan cara membuat serupa dengan jam digital.
+Dia nge-get dari new Date() lalu dijadikan string awalnya. Itu kan statis mas tapi.
+Nah, dia setelah itu buat componentDidMount sama componentWillUnmount.
+componentDidMount tugasnya buat new Date() dan setting interval sedangkan componentwillUnmount clearInterval.
+Pertanyaan saya,
+- Berarti dia setiap detik render ulang kah? refresh page terus-terusan begitu apa baik untuk web?
+- nge-get menit dan detik tertentu itu apa hanya pakai if lalu kalau memenuhi di push ke array kosong saja kah mas? atau ada cara lain di react ini?
+
+(2). Powerful UI?
+Saya sempat baca dokumentasi yang mengatakan bahwa react itu bisa digunakan untuk membuat UI yang powerful.
+Web yang pakai react itu harus nge-get semua data dulu batu ditampilkan dilayar. Lalu, untuk pemrosesan apapun
+yang ada di web tersebut dijalankan lewat "belakang"
+Pertanyaan saya,
+- Benarkah pernyataan diatas?
+
+(3). Yarn?
+Menurut dokumentasi, Yarn itu, "a package manager for node.js"
+Pertanyaan saya,
+- apa npm juga serupa dengan Yarn?
+
+(4). Non-javascript assets?
+"CSS, web fonts, images, and svg can be used as normally in HTML, but can also be 
+loaded through the module system if they are included in the app's `src` directory."
+Menurut dokumen diatas ituuu, "How does React handle non-javascript assets like CSS and web fonts?"
+- saya belum mudeng mas ._."
+
+(5). Pertanyaan?
+"On the initial page load, how does the browser get the initial HTML for 
+the react app's web page?"
+
+-Saya ingin tahu jawaban pertanyaan diatas secara detil mas m(_ _)m
+
+5 Pertanyaan diatas saya temukan waktu belajar liburan kemarin mas.
+Kalau mas baron ndak sibuk bisa tolong dibantu jawab mas? hehe,
+liburan ini saya juga masih mau cari jawabannya mas, kalau ndak 
+ketemu yaa saya tanya mas lagi m(_ _)m
+*/
